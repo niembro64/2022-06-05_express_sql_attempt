0e3329bb89197be6e276914d7f91e960b232548f
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-06-05 20:55:08 -0400

    get working

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -3,8 +3,17 @@ const db = require("../database");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-    res.send("respond with a resource");
+// router.get("/", function (req, res, next) {
+//     res.send("respond with a resource");
+// });
+
+router.get("/", async (req, res) => {
+    const results = await db.promise().query(`SELECT * FROM USERS`);
+    // console.log(results);
+    // console.log("-----------------------------")
+    // console.log(results[0][1].username);
+    // res.send(200);
+    res.status(200).send(results[0]);
 });
 
 router.post("/", (req, res) => {
